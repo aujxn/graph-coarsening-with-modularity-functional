145fa751c7b3e7ba2869dac70c22d996225a5e1c
aujxn <ajn6@pdx.edu>
2019-10-16 23:35:22 -0700

    documented

--- src/main.rs
+++ src/main.rs
@@ -5,72 +5,36 @@ use std::{collections::HashMap, fs, path::Path};
 fn main() {
     let mut data = Data::load();
     for _ in 0..10 {
-        data.coursen();
+        data.coarsen();
     }
 
     data.print_clusters();
-    /*
-        let mod_sums: Vec<f64> = mod_mat
-            .outer_iterator()
-            .map(|row_vec| row_vec.iter().fold(0.0, |acc, (_, val)| acc + val))
-            .collect();
-
-    */
-
-    //   println!("mod sums: {:?}", mod_sums);
-
-    /*
-    let p2: Vec<Option<usize>> = qc1
-        .outer_iterator()
-        .map(|row_vec| {
-            row_vec
-                .iter()
-                .fold((None, 0.0), |(col, max), (j, q)| {
-                    if *q > 0.0 && *q > max {
-                        (Some(j), *q)
-                    } else {
-                        (col, max)
-                    }
-                })
-                .0
-        })
-        .collect();
-
-    let pairs2: Vec<(usize, usize)> = p2
-        .iter()
-        .enumerate()
-        .filter_map(|(i, j)| match j {
-            None => None,
-            Some(j) => {
-                if *j < i || p[*j].is_none() {
-                    None
-                } else if p[*j].unwrap() != i {
-                    None
-                } else {
-                    Some((i, *j))
-                }
-            }
-        })
-        .collect();
-
-    println!("{:?}", pairs2);
-    for (x, y) in pairs2 {
-    }
-    */
 }
 
 struct Data {
-    tweets: Vec<Vec<String>>,
-    word_map: HashMap<String, usize>,
+    /* the vertices of the unprocessed graph */
     word_vec: Vec<String>,
+    /* index is number of passes through the coarsening algo */
     partitions: Vec<Partition>,
 }
 
 struct Partition {
+    /* first index is location on original graph and second is the group of vertices */
     aggregates: Vec<Vec<usize>>,
+    /* CSR graph of the undirected edges with weights */
     graph: CsMat<usize>,
-    delta_Q: CsMat<f64>,
-    row_sums: Vec<usize>,
+    /* change in modularity matrix resulting from modularity functional */
+    delta_q: CsMat<f64>,
+}
+
+impl Partition {
+    /* row sums of the coarsened graph */
+    fn row_sums(&self) -> Vec<usize> {
+        self.graph
+            .outer_iterator()
+            .map(|row_vec| row_vec.iter().fold(0, |acc, (_, val)| acc + val))
+            .collect()
+    }
 }
 
 impl Data {
@@ -85,12 +49,14 @@ impl Data {
         })
     }
 
-    fn coursen(&mut self) {
+    /* performs one level of coarsening on the graph */
+    fn coarsen(&mut self) {
         let level = self.partitions.len() - 1;
         let n = self.partitions[level].aggregates.len();
 
+        /* p is a vector of vertex indices that a given vertex wants to join with */
         let p: Vec<Option<usize>> = self.partitions[level]
-            .delta_Q
+            .delta_q
             .outer_iterator()
             .enumerate()
             .map(|(i, row_vec)| {
@@ -108,6 +74,9 @@ impl Data {
             })
             .collect();
 
+        /* pairs of vertices to merge are found where p_i = j and p_j = i
+         * (where vertices want to join with each other)
+         */
         let pairs: Vec<(usize, usize)> = p
             .iter()
             .enumerate()
@@ -125,43 +94,46 @@ impl Data {
             })
             .collect();
 
+        /* vector of super vertices - vertices that are grouped together */
         let mut aggregates: Vec<Vec<usize>> =
             vec![vec![]; self.partitions[level].aggregates.len() - pairs.len()];
+
+        /* the size of the coarsened graph */
         let nc = n - pairs.len();
-        let mut P = CsMat::zero((n, nc));
+
+        /* piecewise constant interpolation/prolongation matrix - the coarsening transformation matrix */
+        let mut pci = CsMat::zero((n, nc));
         let pairs_len = pairs.len();
 
+        /* group all the pairs and create the transformation matrix */
         for (i, (x, y)) in pairs.iter().enumerate() {
             aggregates[i] = self.partitions[level].aggregates[*x]
                 .iter()
                 .chain(self.partitions[level].aggregates[*y].iter())
                 .map(|z| *z)
                 .collect();
-            P.insert(*x, i, 1.0);
-            P.insert(*y, i, 1.0);
+            pci.insert(*x, i, 1.0);
+            pci.insert(*y, i, 1.0);
         }
 
-        println!("{:?}", pairs);
-        /*
-        for (x, y) in pairs {
-        println!("{:?} - {:?}", data.word_vec[x], data.word_vec[y]);
-        }
-        */
-
+        /* filter out all the paired vertices */
         let singles: Vec<usize> = (0..n)
             .filter(|x| !pairs.iter().any(|(i, j)| x == i || x == j))
             .collect();
 
+        /* copy over the non-paired vertex indices */
         for (j, i) in singles.iter().enumerate() {
             aggregates[j + pairs_len] = self.partitions[level].aggregates[*i]
                 .iter()
                 .map(|z| *z)
                 .collect();
-            P.insert(*i, j + pairs_len, 1.0);
+            pci.insert(*i, j + pairs_len, 1.0);
         }
 
+        /* to new coarsened graph */
         let mut graph = CsMat::zero((nc, nc));
 
+        /* for each pair of aggregates calculate the edge weights */
         (0..nc).into_iter().for_each(|i| {
             (0..nc).into_iter().for_each(|j| {
                 let edge_weight = aggregates[i].iter().fold(0, |acc, x| {
@@ -176,24 +148,20 @@ impl Data {
             })
         });
 
-        let row_sums: Vec<usize> = graph
-            .outer_iterator()
-            .map(|row_vec| row_vec.iter().fold(0, |acc, (_, val)| acc + val))
-            .collect();
-
-        let qp = &self.partitions[level].delta_Q * &P;
-        let delta_Q = &P.transpose_into() * &qp;
+        /* calculate the new delta modularity matrix */
+        let qp = &self.partitions[level].delta_q * &pci;
+        let delta_q = &pci.transpose_into() * &qp;
 
         let coarse = Partition {
             aggregates,
             graph,
-            delta_Q,
-            row_sums,
+            delta_q,
         };
 
         self.partitions.push(coarse);
     }
 
+    /* loads in some text into a graph */
     fn load() -> Self {
         let mut word_map = HashMap::new();
         let mut word_vec = vec![];
@@ -204,6 +172,7 @@ impl Data {
         let mut rdr = csv::Reader::from_reader(contents.as_bytes());
         let mut i: usize = 0;
 
+        /* takes each tweet and puts all the words into its own vec */
         let tweets: Vec<Vec<String>> = rdr
             .records()
             .take(1000)
@@ -215,6 +184,7 @@ impl Data {
             })
             .collect();
 
+        /* iterate over all words and only saves the unique */
         tweets.iter().for_each(|tweet| {
             tweet.iter().for_each(|word| {
                 let owned = String::from(word);
@@ -226,18 +196,22 @@ impl Data {
             })
         });
 
+        /* number of vertices in unprocessed graph */
         let n = word_vec.len();
         let mut graph = CsMat::zero((n, n));
 
+        /* iterates over every word and records what words are found in proximity */
         for tweet in tweets.iter() {
             let len = tweet.len();
             for (x, word) in tweet.iter().enumerate() {
                 match word_map.get(word) {
                     Some(i) => {
+                        /* looks at the next 10 words, so words within 20 words get an edge */
                         for y in (x + 1..x + 10).filter(|y| *y < len) {
                             match word_map.get(&tweet[y]) {
                                 Some(j) => {
                                     let val;
+                                    /* get the current edge value and increment */
                                     match graph.get(*i, *j) {
                                         Some(edge) => val = edge + 1,
                                         None => val = 1,
@@ -245,47 +219,50 @@ impl Data {
                                     graph.insert(*i, *j, val);
                                     graph.insert(*j, *i, val);
                                 }
-                                None => println!("{:?} not found", y),
+                                None => unreachable!(),
                             }
                         }
                     }
-                    None => println!("{:?} not found", x),
+                    None => unreachable!(),
                 }
             }
         }
 
+        /* calculates row sums */
         let row_sums: Vec<usize> = graph
             .outer_iterator()
             .map(|row_vec| row_vec.iter().fold(0, |acc, (_, val)| acc + val))
             .collect();
 
+        /* total of all weights to normalize row sums */
         let total: usize = row_sums.iter().sum();
 
-        let mut delta_Q = CsMat::zero((n, n));
+        /* change in modularity matrix */
+        let mut delta_q = CsMat::zero((n, n));
 
+        /* generate change in modularity matrix */
         graph.outer_iterator().enumerate().for_each(|(i, row_vec)| {
             row_vec.iter().for_each(|(j, data)| {
                 let b: f64 =
                     *data as f64 - (row_sums[i] as f64 * row_sums[j] as f64 / total as f64);
-                let delta_q = 2.0 * b / total as f64;
-                delta_Q.insert(i, j, delta_q);
+                let delta_q_ij = 2.0 * b / total as f64;
+                delta_q.insert(i, j, delta_q_ij);
             })
         });
 
+        /* first group of aggregates has one vertex per aggregate */
         let aggregates: Vec<Vec<usize>> = (0..n).into_iter().map(|i| vec![i]).collect();
 
+        /* unprocessed graph */
         let finest = Partition {
             aggregates,
             graph,
-            delta_Q,
-            row_sums,
+            delta_q,
         };
 
         let partitions = vec![finest];
 
         Data {
-            tweets,
-            word_map,
             word_vec,
             partitions,
         }
